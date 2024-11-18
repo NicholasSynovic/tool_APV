cfc332d55ac821d8584ab1fdc87b221164f3dc20
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-11-18 14:11:56 -0600

    Add API docs

--- apv/download.py
+++ apv/download.py
@@ -11,6 +11,14 @@ AUTHOR_URL_TEMPLATE: string.Template = string.Template(
 def getAuthorPublications(
     authorID: str,
 ) -> requests.Response | typing.Literal[False]:  # noqa: E501
+    """
+    Download an author's public ACM DL publications HTML page
+
+    :param authorID: An ACM author ID
+    :type authorID: str
+    :return: A requests.Response object of the HTML GET request, or False if the status code is not 200
+    :rtype: requests.Response | typing.Literal[False]
+    """  # noqa: E501
     authorURL: str = AUTHOR_URL_TEMPLATE.substitute(AUTHOR_ID=authorID)
     resp: requests.Response = requests.get(url=authorURL, timeout=60)
 
--- apv/htmlParser.py
+++ apv/htmlParser.py
@@ -2,6 +2,16 @@ from bs4 import BeautifulSoup, ResultSet, Tag
 
 
 def _readDiv(soup: BeautifulSoup, divID: str) -> ResultSet[Tag]:
+    """
+    Given an HTML div tag, return a set of all the span tags underneath it
+
+    :param soup: BeuatifulSoup HTML object
+    :type soup: BeautifulSoup
+    :param divID: The id attribute of a div object
+    :type divID: str
+    :return: A set of all span tags that are children of the div tag
+    :rtype: ResultSet[Tag]
+    """
     div: Tag = soup.find(
         name="div",
         attrs={"id": divID},
@@ -10,8 +20,24 @@ def _readDiv(soup: BeautifulSoup, divID: str) -> ResultSet[Tag]:
 
 
 def readJournalMagazineNames(soup: BeautifulSoup) -> ResultSet[Tag]:
+    """
+    Return the list of journal and magazine names that an author has published in
+
+    :param soup: BeuatifulSoup HTML object
+    :type soup: BeautifulSoup
+    :return: The set of all journal and magazine span tags
+    :rtype: ResultSet[Tag]
+    """  # noqa: E501
     return _readDiv(soup=soup, divID="Journal/MagazineNames")
 
 
 def readProceedingsBookNames(soup: BeautifulSoup) -> ResultSet[Tag]:
+    """
+    Return the list of proceeding and book names that an author has published in
+
+    :param soup: BeuatifulSoup HTML object
+    :type soup: BeautifulSoup
+    :return: The set of all proceeding and book span tags
+    :rtype: ResultSet[Tag]
+    """  # noqa: E501
     return _readDiv(soup=soup, divID="Proceedings/BookNames")
